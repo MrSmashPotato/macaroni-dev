7d767ad04fece89bd8ee359eacb2ac2a62791446
NykuluzC <carlslasier1@gmail.com>
2025-05-06 18:07:35 +0800

    Implemented Popup for Editing and Rejecting Job

---
+++ Views/Popup/EditJobApplicationPopup.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using macaroni_dev.Models;
+using macaroni_dev.ViewModels;
+
+namespace macaroni_dev.Views.Popup;
+
+public partial class EditJobApplicationPopup
+{
+    private EditJobApplicationViewModel vm;
+    public EditJobApplicationPopup(User user, JobApplicantsViewModel val, JobPost jobPost)
+    {
+        vm = new EditJobApplicationViewModel(user, val, jobPost);
+        BindingContext = vm;
+        InitializeComponent();
+    }
+}
